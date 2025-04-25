aa26749902747a945874b04d0de0ecc55cd6c941
Antonio Robles <antoniorobles@antonioroblesai.local>
2025-04-25 13:12:50 -0700

    202504251312

--- pull.sh
+++ pull.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-# Stop all PM2 processes
-pm2 stop all
-
 # Pull the latest changes from the Git repository
 git pull
+# Check if there are any changes pulled from the repository
+if [ "$(git diff --name-only HEAD@{1} HEAD)" != "" ]; then
+    echo "Changes detected. Proceeding with npm install and build."
+    # Stop all PM2 processes
+    pm2 stop all
+    # Install dependencies
+    npm install
 
-# Install dependencies
-npm install
+    # Compile the project
+    npm run build
 
-# Compile the project
-npm run build
-
-# Start all PM2 processes
-pm2 start all
+    # Start all PM2 processes
+    pm2 start all
+else
+    echo "No changes detected. Skipping npm install and build."
+    exit 0
+fi
