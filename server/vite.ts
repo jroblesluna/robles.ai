e4a05b2fd14eeca0361708470a1534091aaeaa19
Antonio Robles <antoniorobles@antonioroblesai.local>
2025-04-26 20:50:00 -0700

    202504262050

--- server/vite.ts
+++ server/vite.ts
@@ -78,7 +78,7 @@ export function serveStatic(app: Express) {
     );
   }
 
-  app.use(express.static(distPath));
+  app.use(express.static(distPath, { maxAge: "1y", immutable: true }));
 
   // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
